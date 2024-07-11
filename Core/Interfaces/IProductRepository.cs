570b50e849c1ae679096dda5d61b0c68dc60ddfa
Narendrareddyanumula <naren.anumula@gmail.com>
2024-07-11 11:56:04 +0530

    Add Core project with Product entity and IProductRepository interface

---
+++ Core/Interfaces/IProductRepository.cs
@@ -0,0 +1,15 @@
+// Core/Interfaces/IProductRepository.cs
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAllAsync();
+        Task<Product> GetByIdAsync(int id);
+        Task AddAsync(Product product);
+        Task UpdateAsync(Product product);
+        Task DeleteAsync(int id);
+    }
+}
